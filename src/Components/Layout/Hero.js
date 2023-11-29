936d2219d8eee6047712d4c16dc0c84cdb7a7b35
FA <fabricecoder009@gmail.com>
2023-11-29 08:36:27 +0200

    customizing hero.js and homeMenu.js

--- src/Components/Layout/Hero.js
+++ src/Components/Layout/Hero.js
@@ -9,23 +9,24 @@ export default function Hero(){
           <p className="my-6 text-gray-500 text-sm">Pizza is the missing piece that makes every<br />day complete, a simple yet delicious joy in life</p>
           </div>
     
-          <div className="relative">
+          <div className="relative right-330">
           <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'} />
           </div>
-          <div className="">
-          <div className="flex gap-4 text-sm">
-  <button className="bg-primary uppercase flex items-center gap-2 text-white px-6 py-2 rounded-full">
-  <span className=" whitespace-nowrap">Order now</span>
-   <span className="flex-1 w-7 h-7"><Right /></span>
-  </button>
-  <button className="flex items-center gap-2 text-gray-600 py-2 font-semibold">
-  <span className="whitespace-nowrap">Learn more</span>
-  <span className="flex-1 w-7 h-7 flex items-center"><Right /></span>
-  </button>
-</div> 
-          </div>
-
-          
+          <div className="absolute  right-0">
+                   <Image src={'/sallad2.png'}width={107} height={220}  alt={'sallad'} />
+                   </div>
+          <div className="mt-[-50px]">
+  <div className="flex gap-4 text-sm">
+    <button className="bg-primary uppercase flex items-center gap-2 text-white px-6 py-2 rounded-full">
+      <span className="whitespace-nowrap">Order now</span>
+      <span className="flex-1 w-7 h-7"><Right /></span>
+    </button>
+    <button className="flex items-center gap-2 text-gray-600 py-2 font-semibold">
+      <span className="whitespace-nowrap">Learn more</span>
+      <span className="flex-1 w-7 h-7 flex items-center"><Right /></span>
+    </button>
+  </div>
+</div>
      </section>
      );
 }
