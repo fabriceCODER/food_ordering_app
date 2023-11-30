0b4248655fc88ab1060f440d1b589c5324490b3b
FA <fabricecoder009@gmail.com>
2023-11-30 16:14:49 +0200

    Customizing styles

--- src/app/Register/page.js
+++ src/app/Register/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function RegisterPage()
 {
    return(
@@ -9,6 +11,15 @@ export default function RegisterPage()
           <input type="email" placeholder="Email"/>
             <input type="password" placeholder="Password" />
             <button type="submit">Register</button>
+             <div className="my-4 text-center text-gray-500">
+               or login with provider
+             </div>
+            <button className="flex border gap-4 px-2
+             border-gray-700 rounded-xl justify-center
+              text-gray-700 mt-4 w-full py-2">
+               <Image src={'/google.png'} alt={'login with google.com'} width={24} height={32}  />
+               Login with Google
+               </button>
           </form>
       </section>
    )
