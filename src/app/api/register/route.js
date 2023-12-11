d5c5f27c6aa86ee51c6d634d164b9fe50482eca0
FA <fabricecoder009@gmail.com>
2023-12-11 21:55:46 +0200

    Customizing auth logic

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,3 +4,28 @@ import User from "./User"; // Assuming you have a User model file
 export async function POST(req) {
   try {
     const body = await req.json();
+
+    // Ensure Mongoose is connected before attempting to create a user
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const createUser = await User.create(body);
+
+    // Close the Mongoose connection after creating the user
+    await mongoose.connection.close();
+    
+    return {
+      status: 200,
+      body: { message: "User created successfully", user: createUser },
+    };
+  } catch (error) {
+    console.error("Error creating user:", error);
+
+    return {
+      status: 500,
+      body: { message: "Internal Server Error" },
+    };
+  }
+}
