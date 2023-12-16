c9d400339d0fd1f0e3713d5735779581b9f32d53
FA <fabricecoder009@gmail.com>
2023-12-16 15:19:12 +0200

    Auth logic customization

--- src/app/Register/page.js
+++ src/app/Register/page.js
@@ -36,12 +36,17 @@ export default function RegisterPage()
     onChange={(ev) => setPassword(ev.target.value)}
   />
   <button type="submit">Register</button>
-  <div className="my-4 text-center text-gray-500">or login with provider</div>
-  <button className="flex border gap-4 px-2 border-gray-700 rounded-xl justify-center text-black-700 mt-4 w-full py-2">
-    <Image src={'/google.png'} alt={'login with google.com'} width={24} height={32} />
+  <div className="my-4 text-center text-gray-500">
+   or login with provider</div>
+  <button className="flex border gap-4 px-2
+   border-gray-700 rounded-xl justify-center
+    text-black-700 mt-4 w-full py-2">
+    <Image src={'/google.png'} 
+    alt={'login with google.com'}
+    width={24} height={32} />
     Login with Google
   </button>
 </form>
       </section>
-   )
+   )  
 }
